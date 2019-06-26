681d80d2d5347a066d69875f39585afb56ca46f5
Michael Nguyen <michaelnguyenz229@gmail.com>
2019-06-25 20:55:54 -0700

    first version of first practice

---
+++ first_practive.sv
@@ -0,0 +1,16 @@
+module my_and_gate
+(
+    input a,
+    input b,
+    output y,
+    output x,
+    output z,
+    output f
+);
+
+assign y = a & b;
+assign x = a | b;
+assign z = a ^ b;
+assign f = ~(a & b);
+endmodule
+
